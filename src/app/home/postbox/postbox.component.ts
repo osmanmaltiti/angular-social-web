798c97467a52ad077c76d7972fc351747ab1ed37
osmanmaltiti <terabyte7778@gmail.com>
2022-07-09 22:37:08 -1200

    color changes

--- src/app/home/postbox/postbox.component.ts
+++ src/app/home/postbox/postbox.component.ts
@@ -1,4 +1,12 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
 import { CreatePostService } from './../../services/create-post.service';
 
 @Component({
@@ -8,6 +16,8 @@ import { CreatePostService } from './../../services/create-post.service';
 })
 export class PostboxComponent implements OnInit {
   @Output('oncreatepost') oncreatepost: EventEmitter<any> = new EventEmitter();
+  @Output('clickoutside') clickOutside: EventEmitter<any> = new EventEmitter();
+  @ViewChild('outsidepostbox') outsidepostbox: ElementRef | undefined;
   @Input('setOpen') setOpen: boolean = false;
   post: string = '';
 
@@ -18,6 +28,13 @@ export class PostboxComponent implements OnInit {
       next: () => this.oncreatepost.emit(),
       error: ({ error }) => console.log(error),
     });
+  }
+
+  onClickOutside(event: Event) {
+    if (<HTMLElement>event.target === this.outsidepostbox?.nativeElement) {
+      this.setOpen = false;
+      this.clickOutside.emit();
+    }
   }
   ngOnInit(): void {}
 }
